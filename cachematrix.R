84eee249fe274953b1ec412a63ceb51f0dd322e6
amaziz <contact@amaziz.com>
2015-02-18 15:37:49 +0100

    This is the solution for assignment 2 Thank you for reviewing it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This code calculates the inverse of an invertible matrix
+# It consists of 2 functions. 
+# Warning! Code doesn't check whether a matrix is invertible.
+# It just assumes that it is.
+#
+# Example test case:
+# testmatrix = matrix(c(2,2,3,2), nrow=2, ncol=2)
+# preparedmatrix <- makeCacheMatrix(testmatrix)
+# cacheSolve(preparedmatrix)
+# cacheSolve(preparedmatrix)
 
-## Write a short comment describing this function
+
+## This function gets and sets the matrix to be evaluated (called x)
+## The function also gets and set the invese of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         matrixinverse <- NULL
@@ -12,14 +22,16 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setinverse <- function(inverse) matrixinverse <<- inverse
         getinverse <- function() matrixinverse
-        list(set = set, get = get,
+        matrix(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 
 
-## Write a short comment describing this function
+## This function checks if an inverse is calculated
+## If not, it calculates the inverse using the solve() function
+## It thn sets the matrixinverse accirdingly and returns its value
 
 cacheSolve <- function(x, ...) {
         matrixinverse <- x$getinverse()
@@ -31,7 +43,7 @@ cacheSolve <- function(x, ...) {
         matrixinverse <- solve(data)
         x$setinverse(matrixinverse)
         
-          ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
         matrixinverse
 }
 
